be30fe5213b2bae6225f7d27c939a98fae097da0
Salvatore Napoli <snapoli1998@gmail.com>
2021-03-11 21:33:50 +0100

    Predisposizione products

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,19 @@
+<header class="header flex">
+	<div class="logo">
+		<img src="img/marchio-sito-test.png" alt="logo" />
+	</div>
+
+	<nav class="website-nav">
+		<ul class="website-nav-list no-bullet-ul flex">
+			<li class="navigator">
+				<a href="#" class="no-decoration-text">Home</a>
+			</li>
+			<li class="navigator current-section">
+				<a href="#" class="no-decoration-text">Prodotti</a>
+			</li>
+			<li class="navigator">
+				<a href="#" class="no-decoration-text">News</a>
+			</li>
+		</ul>
+	</nav>
+</header>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -132,7 +132,7 @@ $array = json_decode($data, true);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>La Molisana</title>
+    <title>La Molisana - Prodotti</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -141,53 +141,10 @@ $array = json_decode($data, true);
   <body>
     @include('common/header')
 
-    <main class="main">
-      <section class="pasta-category">
-        <h2 class="pasta-title">le lunghe</h2>
-        <div class="pasta flex">
-          @foreach ($array as $pasta_type)
-            @if ($pasta_type['tipo'] === 'lunga')
-            <div class="pasta-item">
-              <a href="#">
-                <img src="@php echo $pasta_type['src'] @endphp" alt="tipo di pasta" />
-              </a>
-            </div>
-            @endif
-          @endforeach
-        </div>
-      </section>
+		<main>
 
-      <section class="pasta-category">
-        <h2 class="pasta-title">le corte</h2>
-        <div class="pasta flex">
-          @foreach ($array as $pasta_type)
-            @if ($pasta_type['tipo'] === 'corta')
-            <div class="pasta-item">
-              <a href="#">
-                <img src="@php echo $pasta_type['src'] @endphp" alt="tipo di pasta" />
-              </a>
-            </div>
-            @endif
-          @endforeach
-        </div>
-      </section>
+		</main>
 
-      <section class="pasta-category">
-        <h2 class="pasta-title">le cortissime</h2>
-        <div class="pasta flex">
-          @foreach ($array as $pasta_type)
-            @if ($pasta_type['tipo'] === 'cortissima')
-            <div class="pasta-item">
-              <a href="#">
-                <img src="@php echo $pasta_type['src'] @endphp" alt="tipo di pasta" />
-              </a>
-            </div>
-            @endif
-          @endforeach
-        </div>
-      </section>
-    </main>
-
-    @include('common/footer')
-  </body>
+		@include('common/footer')
+	</body>
 </html>
