78051f300b85179784641fb1dd517c7d7fae3a0f
Salvatore Napoli <snapoli1998@gmail.com>
2021-03-11 21:02:46 +0100

    Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -200,8 +200,88 @@ $array = json_decode($data, true);
       </section>
     </main>
 
-    <footer>
-      
+    <footer class="footer flex">
+      <div class="website-info">
+        <div class="logo">
+          <img src="img/marchio-sito-test.png" alt="logo" />
+        </div>
+        <div class="contacts flex">
+          <span>Ragione sociale: La Molisana S.P.A.</span>
+          <span>Sede legale: Contrada Colle delle Api, 100/A</span>
+          <span>86100 - Campobasso (CB)</span>
+          <span>Pec: lamolisana@pec.it</span>
+          <span>Tel: +39 0874 4981</span>
+          <span>Fax: +39 0874 629584</span>
+          <span>info@lamolisana.it (per segnalazioni degli utenti)</span>
+          <span>commerciale@lamolisana.it</span>
+          <span>export@lamolisana.it</span>
+          <span>numero verde 800818081</span>
+          <span>telefono 3801292455</span>
+        </div>
+      </div>
+
+      <div class="links">
+        <h2 class="links-category">PASTIFICIO</h2>
+        <ul class="links-list no-bullet-ul">
+          <li class="item">
+            <a href="#" class="no-decoration-text">Il Pastificio</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Grano decorticato a pietra</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Il Molise c'è</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Filiera Integrata</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">100 anni di pasta</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Sartoria della pasta</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Spaghetto Quadrato</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Le Persone</a>
+          </li>
+        </ul>
+
+        <h2 class="links-category">PRODOTTI</h2>
+        <ul class="links-list no-bullet-ul">
+          <li class="item">
+            <a href="#" class="no-decoration-text">Le Classiche</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Le Integrali</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Le Speciali</a>
+          </li>
+          <li class="item"><a href="#" class="no-decoration-text">Le Biologiche</a></li>
+          <li class="item"><a href="#" class="no-decoration-text">Le Gluten-Free</li>
+          <li class="item"><a href="#" class="no-decoration-text">Le Semole</li>
+          <li class="item"><a href="#" class="no-decoration-text">Le Extra di Lusso</li>
+        </ul>
+      </div>
+
+      <div class="links">
+        <h2 class="links-category">COLLEZIONE DA CHEF</h2>
+        <ul class="links-list no-bullet-ul">
+          <li class="item">
+            <a href="#" class="no-decoration-text">Collezione da Chef</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Grandi Cucine</a>
+          </li>
+          <li class="item">
+            <a href="#" class="no-decoration-text">Biologiche</a>
+          </li>
+          <li class="item"><a href="#" class="no-decoration-text">Quadrate</a></li>
+        </ul>
+      </div>
     </footer>
   </body>
 </html>
