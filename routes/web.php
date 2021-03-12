ccf996922bd2ff7a0720c4aeb0a4abe12d51de23
Salvatore Napoli <snapoli1998@gmail.com>
2021-03-12 11:23:05 +0100

    Config array

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $pasta = config('pasta');
+    return view('home',
+      ['pastaArray' => $pasta]
+    );
 });
 
 Route::get('/products/{id}', function ($id) {
+    $pasta = config('pasta');
     return view('products',
-      ['productId' => $id]
+      ['productId' => $id,
+      'pastaArray' => $pasta]
     );
 });
